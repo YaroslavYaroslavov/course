0dd6a4d5fa56ff1eb80c3e5416e8c9d416e8df43
Yaroslav <tvoibaget@mail.ru>
2023-03-16 13:27:36 +0300

    123

--- client.js
+++ client.js
@@ -1,23 +1,29 @@
 const send = document.querySelector('.btn')
-const input = document.querySelector('input')
-const socket = new WebSocket('ws://localhost:8080')
+const input = document.querySelector('.msg')
+const ipInput = document.querySelector('.ip_input')
+const ipBtn = document.querySelector('.btn_ip')
 
+let link = ''
+let socket = {}
+ipBtn.addEventListener('click', () => {
+        link = `ws://${ipInput.value}:8080`
+        socket = new WebSocket(link)
+        let time = null
+        socket.onopen = function(event) {
+            time = new Date()
+            console.log(`Соединение установлено в ${time}`);
+        };
 
-let time = null
-socket.onopen = function(event) {
-    time = new Date()
-    console.log(`Соединение установлено в ${time}`);
-};
+        socket.onmessage = function(event) {
+            console.log('От сервера пришло:', event.data);
+        };
 
-socket.onmessage = function(event) {
-    console.log('От сервера пришло:', event.data);
-};
-
-socket.onclose = function(event) {
-    console.log('Соединение прервано!');
-};
-
-send.addEventListener('click', () => {
-    console.log('Отправлено серверу.')
-    socket.send(input.value.toString())
-})
+        socket.onclose = function(event) {
+            console.log('Соединение прервано!');
+        };
+        send.addEventListener('click', () => {
+            console.log('Отправлено серверу.')
+            socket.send(input.value.toString())
+        })
+    })
+    // const socket = new WebSocket('ws://192.168.43.25:8080')
